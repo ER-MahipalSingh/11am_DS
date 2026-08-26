1a83f1ebb3ac267f56dd57e96e5d9b90ee6d234c
MAHIPAL SINGH <rwr1.mahipal.sb@gmail.com>
2026-08-26 11:53:28 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,33 @@
+# while True:
+#     num = int(input("Enter to exit press 1 "))
+    
+#     if num == 1:
+#         break
+
+
+# while True:
+#     print("----Welcome to ATM----")
+#     print("01. Enter a number: ")
+#     print("02. press to exit: ")
+#     choice = int(input("Enter your Choice: "))
+    
+#     if choice == 2:
+#         break
+
+# num = 1
+# while num <= 10:
+#     print(num, end=" ")
+#     num += 1
+
+
+# for i in range(1,11):
+#     print(i)
+    
+# for i in range(0,6):
+#     print(i, end=" ")
+
+# for i in range(2, 21, 2):
+#     print(i, end=" ")
+
+for i in range(10,0,-1):
+    print(i)

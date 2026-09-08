288b185e11ad6b323b0f6d1ecce7702b4394eb2e
MAHIPAL SINGH <rwr1.mahipal.sb@gmail.com>
2026-09-08 11:43:34 +0530

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,44 @@
+# a = 'hello'
+# b = "Hello Students"
+# c = '''ML Engineer'''
+# d = """Python 
+# Java
+# JavaScript
+# """
+
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+# tech = "python developer"
+
+# print(tech)
+# print(tech[2])
+# print(tech[5])
+
+# for i in tech:
+#     print(i, end=" ")
+
+# print(tech[2:5])
+# print(tech[::5])
+# print(tech[:2:5])
+# print(tech[1:12:2])
+
+# name = "Jhon"
+# age = "30"
+
+# data = "My name is " + name + " " + " and my age is " + age
+# print(f"My name is {name} and age is {age}")
+
+# print(data)
+
+tech = "python developer"
+
+print(len(tech))
+print(tech.upper())

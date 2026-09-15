761fdbec146f1c6070fa7c452e3281d95d6b3562
MAHIPAL SINGH <rwr1.mahipal.sb@gmail.com>
2026-09-15 11:56:17 +0530

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,58 @@
+# a = 10
+
+# def show():
+#     print("Hello")
+#     print("Fun: ",a)
+#     b = 20
+#     print("Fun b:",b)
+    
+
+# show()
+# show()
+# show()
+# show()
+# show()
+# print(a)
+# print(b)
+
+# def display(a):
+#     print(a)
+    
+# display(50)
+# display("Hello")
+
+# def show():
+#     return 10
+
+# res = show()
+# print(res)
+
+# def display(a):
+#     return a
+#     print(a)
+
+# res = display("Hello")
+# print(res)
+
+# def fact(num):
+#     if num == 1: return 1
+#     return num * fact(num -1)
+
+# num = int(input("Enter value: "))
+# res = fact(num)
+# print(res)
+# print(num)
+
+# max = lambda a, b: a > b
+
+# print(max(20,10))
+
+# def show(*num):
+#     print(*num)
+    
+def tech(**data):
+    print(data)
+   
+# res = show(10,20,30, "Hello") 
+tech(tech="python", version=20) 
+# print(res)

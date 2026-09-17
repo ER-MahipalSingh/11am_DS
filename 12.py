2bd480d092d9800c635b6a89b80f088ecb79ee46
MAHIPAL SINGH <rwr1.mahipal.sb@gmail.com>
2026-09-17 11:08:30 +0530

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,46 @@
+# number = [10,5,20,30,40]
+
+# print(type(number))
+
+# for i in number:
+#     print(i, end=" ")
+    
+# num = 30
+# for i in number:
+#     if i == num:
+#         print("Found")
+#         break
+#     else:
+#         print("Not Found")
+
+# max = number[0]
+# min = number[0]
+
+# for i in number:
+#     if max <= i:
+#         max = i
+#     else:
+#         min = i
+
+# print("Max:",max)
+# print("Min:",min)
+
+# data = [
+#     [10,20,30],
+#     [1,2,3],
+#     [100,220,330],
+# ]
+
+# for row in data:
+#     for col in row:
+#         print(col, end=" ")
+#     print()
+
+
+num = [15,2,8,62,88,54,10]
+
+num.sort()
+
+sorNum = sorted(num)
+
+print("Sort:", sorNum)
